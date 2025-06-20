29ef13e393fd6df2cd41144d067ca00136cae00e
Harshita Arora <162481967+harshitaarora0019@users.noreply.github.com>
2025-06-20 15:27:41 +0530

    Create Random-Quote-Generator.py

---
+++ Random-Quote-Generator.py
@@ -0,0 +1,12 @@
+import random
+
+quotes = [
+    "Believe in yourself.",
+    "Push yourself, because no one else is going to do it for you.",
+    "Great things never come from comfort zones.",
+    "Success doesn't just find you. You have to go out and get it.",
+    "Don’t stop when you’re tired. Stop when you’re done."
+]
+
+print("💡 Random Quote:")
+print(random.choice(quotes))
